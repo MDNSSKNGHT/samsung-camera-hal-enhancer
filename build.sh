b569fbecbaad9bf7f3cf33afbc6a372f4f0fb2e1
Leonardo Estacio <mdnssknght@gmail.com>
2022-12-24 18:30:57 -0500

    build: Update to support Zygisk (build and zip it)
    You'll need to download Android NDK (r25) and but it in this
    location: $HOME/android/sdk/ndk/

--- build.sh
+++ build.sh
@@ -2,7 +2,17 @@
 
 set -eu
 
+pushd zygisk/module
+$HOME/android/sdk/ndk/25.1.8937393/ndk-build
+popd
+
 [ -d ".temp" ] && rm -r .temp
-mkdir .temp/ ; cp -r magisk/* .temp/
-cd .temp/ ; zip -r9 Module-Debug.zip *
-mv *.zip .. ; cd .. ; rm -r .temp
+mkdir -p .temp/zygisk ; cp -r magisk/* .temp
+for arch in arm64-v8a armeabi-v7a x86 x86_64; do
+    cp "zygisk/module/libs/$arch/libexample.so" ".temp/zygisk/$arch.so"
+done
+
+pushd .temp
+zip -r9 Module-Debug.zip * ; mv *.zip ..
+popd
+rm -r .temp

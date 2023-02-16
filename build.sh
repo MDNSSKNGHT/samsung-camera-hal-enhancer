e4641dc97a4e1a65838253a7fdc8dc344697374a
Leonardo Estacio <mdnssknght@gmail.com>
2023-02-16 17:57:44 -0500

    Initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -eu
+
+PRODUCT="${1:-none}"
+
+[ -d ".temp" ] && rm -r .temp
+mkdir .temp/ ; cp -r magisk/* product/$PRODUCT/* .temp/
+cd .temp/ ; zip -r9 Module-$PRODUCT.zip *
+mv *.zip .. ; cd .. ; rm -r .temp

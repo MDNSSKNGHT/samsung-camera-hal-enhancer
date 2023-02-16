aa440e6d28827ee5b468d327f6aee06503fe12dd
Leonardo Estacio <mdnssknght@gmail.com>
2023-02-16 18:05:42 -0500

    Don't add devices into product folder

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -eu
 PRODUCT="${1:-none}"
 
 [ -d ".temp" ] && rm -r .temp
-mkdir .temp/ ; cp -r magisk/* product/$PRODUCT/* .temp/
+mkdir .temp/ ; cp -r magisk/* "$PRODUCT"/* .temp/
 cd .temp/ ; zip -r9 Module-$PRODUCT.zip *
 mv *.zip .. ; cd .. ; rm -r .temp
